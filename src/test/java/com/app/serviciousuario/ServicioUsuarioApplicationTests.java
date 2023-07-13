8207f6e42e8265974e28035158d979a27b528c98
Kairos Jesus Adrian Diaz Orozco <nem.jadrian@coppel.com>
2023-07-13 15:23:13 -0700

    Inicializando proyecto

---
+++ src/test/java/com/app/serviciousuario/ServicioUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.serviciousuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
